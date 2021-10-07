2ea3a726e68962c1a2994d5689639eb93a2661c7
Harun Çınar <55881311+haruncinar@users.noreply.github.com>
2021-10-07 22:31:36 +0300

    Add files via upload

---
+++ src/main/java/com/cinar/mongoInSB/entity/Student.java
@@ -0,0 +1,29 @@
+package com.cinar.mongoInSB.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Data
+@Document(collection = "student")
+public class Student
+{
+    //AUTO-GENERATE managed by MongoDB
+    @Id
+    private String id;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "mail")
+    private String email;
+
+    @Field(name = "department")
+    private Department department;
+
+    @Field(name = "subjects")
+    private List<Subject> subjects;
+}
